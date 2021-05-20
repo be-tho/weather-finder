5b25eeac8d339c0867446060fdd822328d21ff88
be-tho <yiyevp@gmail.com>
2021-05-20 11:31:42 -0300

    vinculando datos
    Vinculando los datos con los datos del fetch, Chekeado funciona.
    empezando un poco con los estilos.

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ const app = new Vue({
        return{
            ciudadBuscar: "",
            clima: {
-               nombreCiudad: "london",
-               pais: "UK",
+               nombreCiudad: "Buenos Aires",
+               pais: "ARG",
                temperatura: 12,
                descripcion: "Muchas Nubes",
                tempBaja: "8",
@@ -18,13 +18,32 @@ const app = new Vue({
    },
     methods:{
        /* La funcion donde hacemos el fetch*/
-       getClima(){
+        getClima: async function (){
            console.log("Obteniando el click del form para ejecutar el fetch aca.");
 
-           // Peticion al end Point de weather
+           //Datos para la petición
            console.log(this.ciudadBuscar);
            const APPI_KEY = "c6f6c8cacc4c4b6d31edf9d80cd34c25";
            const URL = `https://api.openweathermap.org/data/2.5/weather?q=${this.ciudadBuscar}&appid=${APPI_KEY}&units=metric`;
+
+
+           // Petición al end Point de weather
+           const response = await fetch(URL);
+           const data = await response.json();
+           console.log(data);
+
+           //Asignarle los datos del json a cada valor de nuestra app
+           this.ciudadBuscar = "";
+           this.clima.nombreCiudad = data.name;
+           this.clima.pais = data.sys.country;
+           this.clima.temperatura = Math.round(data.main.temp);
+           this.clima.descripcion = data.weather[0].description;
+           this.clima.tempBaja = Math.round(data.main.temp_min);
+           this.clima.tempAlta = Math.round(data.main.temp_max);
+           this.clima.feelsLike = Math.round(data.main.feels_like);
+           this.clima.humedad = Math.round(data.main.humidity);
+
+           const tiempoDeDia = data.weather[0].icon;
        }
     }
 });
